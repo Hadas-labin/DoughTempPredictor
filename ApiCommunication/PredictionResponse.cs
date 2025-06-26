872963fc1ae5bbc074b8cae240812010dde8135f
Hadas Labin <H7660448@gmail.com>
2025-06-26 16:37:12 +0300

    Initial commit

---
+++ ApiCommunication/PredictionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.ApiCommunication
+{
+    public class PredictionResponse
+    {
+        public double IceAmount { get; set; }
+        public double? OilAmount { get; set; }
+        public string? ErrorMessage { get; set; } = "";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using DoughTempPredictor.RegressionCalculator;
+using System.Text.Json;
+using project.ApiCommunication;
+using DoughTempPredictor;
+
+namespace DoughTempPredictor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReact", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseCors("AllowReact");
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapPost("/send", (TemperatureRequest data) =>
+            {
+                try
+                {
+                    double[] inputs = new double[] { data.flour, data.oil, data.water, 0 };
+                    (double IceAmount, double? OilAmount) = PredictionEnhancements.CheckAndSendFeature(inputs, 120);
+
+                    var response = new PredictionResponse
+                    {
+                        IceAmount = IceAmount,
+                        OilAmount = OilAmount,
+                        ErrorMessage = ""
+                    };
+
+                    return Results.Ok(response);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                    return Results.Ok(new PredictionResponse
+                    {
+                        ErrorMessage = ex.Message
+                    });
+                }
+            });
+
+            app.MapPost("/manual-predict", async (HttpContext context) =>
+            {
+                try
+                {
+                    // כאן תבצע את פעולת החיזוי – לדוגמה:
+                    Console.WriteLine("📦 התחיל חיזוי חודשי לפי נתוני בצק");
+
+                    // קריאה לפונקציית חיזוי כלשהי
+                    LinearRegression.PerformGradientDescent();
+
+                    predict.PredictAndPrintFromCsv();
+
+                    // מחזירים תגובה ריקה עם סטטוס 204 (No Content)
+                    return Results.NoContent();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine( ex.Message);
+                    return Results.StatusCode(500); // Internal Server Error
+                }
+            });
+
+
+            app.Run();
+        }
+    }
+}
